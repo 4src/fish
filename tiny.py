84b7b765ca268b5e7c31444f24671ec27ecc6a0e
Tim Menzies <timm@ieee.org>
2023-06-24 04:51:54 -0400

    saving

--- tiny.py
+++ tiny.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3 -B
 #vim: set et sts=2 sw=2 ts=2 : 
+# https://en.wikipedia.org/wiki/Effect_size#Other_metrics
 from fileinput import FileInput as file_or_stdin
 import traceback,random,math,sys,re
 from termcolor import colored
@@ -10,7 +11,8 @@ class BAG(dict):
   __getattr__ = dict.get
   __repr__    = lambda i: str({k:i[k] for k in i.__dict__ if not k[0].isupper()})
 
-the=BAG(seed=1234567890, file="../data/auto93.csv")
+the=BAG(seed=1234567890, 
+        file="../data/auto93.csv")
 
 #----------------------------------------------------------------------------------------
 def DATA(data=None,rows=[]):
@@ -19,7 +21,7 @@ def DATA(data=None,rows=[]):
   return data
 
 def COLS(names):
-  x,y,all,klass, = [], [], [COL(at,txt) for at,txt in enumerate(names)],None
+  x,y,klass,all = [], [], None, [COL(at,txt) for at,txt in enumerate(names)]
   for col in all:
     z= col.txt[-1]
     if z != "X":
@@ -34,7 +36,7 @@ def SYM(at,txt):
   return BAG(This=SYM,at=at,txt=txt,n=0, has={},most=0,mode=None)
 
 def NUM(at,txt):
-  return BAG(This=NUM, at=at, txt=txt, n=0, mu=0, hi= -1E30, lo=1E30,
+  return BAG(This=NUM, at=at, txt=txt, n=0, mu=0, m2=0,  hi= -1E30, lo=1E30,
              w= 0 if txt[-1]=="-" else 1)
 #----------------------------------------------------------------------------------------
 def adds(data,row):
@@ -44,9 +46,11 @@ def adds(data,row):
 
 def add(col,x):
   def num():
-    col.hi  = max(x, col.hi)
-    col.lo  = min(x, col.lo)
-    col.mu += (x - col.mu)/col.n
+    col.lo = min(x, col.lo)
+    col.hi = max(x, col.hi)
+    d       = x - col.mu
+    col.mu += d/col.n
+    col.m2 += d*(x - col.mu)
   def sym():
     tmp = col.has[x] = 1 + col.has.get(x,0)
     if tmp > col.most: col.most, col.mode = tmp, x
@@ -68,7 +72,20 @@ def better(data,row1,row2): return d2h(data,row1) < d2h(data,row2)
 
 def ordered(data,rows=None):
   return sorted(rows or data.rows, key=cmp_to_key(lambda a,b: better(a,b)))
- #----------------------------------------------------------------------------------------
+
+def mid(col): 
+  return col.mode if col.This is SYM else col.mu
+
+def div(col):
+  log2 = lambda x: math.log(x,2)
+  p    = lambda k: col.has[k]/col.n
+  return -sum((p(k)*log2(p(k)) for k in d) if col.This is SYM else (col.m2/(col.n - 1))**.5
+
+def stats(data, cols=None, fun=mid):
+  tmp = {col.txt: fun(col) for col in (cols or data.cols.y)}
+  tmp["N"] = len(data.rows)
+  return BAG(**tmp)
+#----------------------------------------------------------------------------------------
 def coerce(x):
   try: return literal_eval(x)
   except: return x
