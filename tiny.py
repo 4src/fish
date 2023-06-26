34713b9c296a5ea73a23c8dd0d921cd0fd7ec7e1
Tim Menzies <timm@users.noreply.github.com>
2023-06-26 18:11:12 -0400

    saving

--- tiny.py
+++ tiny.py
@@ -7,15 +7,15 @@ from termcolor import colored
 from functools import cmp_to_key
 from ast import literal_eval
 
-#class BAG(object):
-class BAG(object): 
+#class BOX(object):
+class BOX(object): 
   def __init__(i,**d): i.__dict__.update(**d)
   def __repr__(i):
     show= lambda x: x.__name__ if callable(x) else (f"{x:3g}" if isinstance(x,float) else x)
     d = i.__dict__
     return "{"+" ".join([f":{k} {show(d[k])}" for k in d if  k[0] != "_"])+"}"
 
-the=BAG(seed = 1234567891, 
+the=BOX(seed = 1234567891, 
         min  = .5,
         rest = 3,
         file = "../data/auto93.csv")
@@ -24,10 +24,10 @@ def COL(at=0,name=""):
   return (NUM if name and name[0].isupper() else SYM)(at=at, name=name)
 
 def SYM(at=0,name=""):
-  return BAG(this=SYM, name=name, at=at, n=0, has={}, most=0, mode=None, filter=lambda x:x)
+  return BOX(this=SYM, name=name, at=at, n=0, has={}, most=0, mode=None, filter=lambda x:x)
 
 def NUM(at=0,name=""):
-  return BAG(this=NUM, name=name, at=at, n=0, mu=0, m2=0, hi= -1E30, lo=1E30, 
+  return BOX(this=NUM, name=name, at=at, n=0, mu=0, m2=0, hi= -1E30, lo=1E30, 
              filter=int,
              want= 0 if (name and name[-1]) =="-" else 1)
 
@@ -39,10 +39,10 @@ def COLS(names):
     if z != "X":
       (y if z in "+-!" else x).append(col)
       if z=="!": klass=col
-  return BAG(this=COLS, x=x, y=y, names=names, all=all,klass=klass)
+  return BOX(this=COLS, x=x, y=y, names=names, all=all,klass=klass)
 
 def DATA(rows=[], data=None):
-  data = data or BAG(this=DATA,rows=[],cols=None)
+  data = data or BOX(this=DATA,rows=[],cols=None)
   [adds(data,lst) for lst in rows]
   return data
 #----------------------------------------------------------------------------------------
@@ -97,69 +97,75 @@ def ordered(data,rows=None):
   return sorted(rows or data.rows, key=lambda r: d2h(data,r))
 
 def stats(data, cols=None, fun=lambda c: mid(c)):
-  return BAG(N=len(data.rows), **{col.name: fun(col) for col in (cols or data.cols.y)})
+  return BOX(N=len(data.rows), **{col.name: fun(col) for col in (cols or data.cols.y)})
 
 def clone(data,rows=[]):
   return DATA(data=DATA(rows=[data.cols.names]),rows=rows)
 #----------------------------------------------------------------------------------------
-def CRITERION(col,lo,hi) : 
-  return BAG(this=CRITERION, score=0,at=col.at, lo=lo, hi=hi, name=col.name)
+def KEY(col,lo,hi):
+  return BOX(this=KEY, at=col.at, lo=lo, hi=hi, name=col.name)
 
-def CRITERIA(): return BAG(this=CRITERIA, all=[])
+def RULE(keys=[]):
+  rule = BOX(this=RULE, score = 0, cols={})
+  [key(rule,k) for k in keys]
+  return rule
 
-def criteria(criteria1, criterion):
-  now = creteria1.all[criterion.at] = criteria1.all.get(criterion.at,[]) 
-  now += [criterion]
+def key(rule, new):
+  if new.this is RULE:
+    [key(rule,k) for lst in new.cols.values() for k in lst]
+  else:
+    b4 = rule.cols[new.at] = rule.cols.get(new.at,[])
+    overlaps = False
+    for  old in b4:
+      if new.lo <= old.lo and old.lo <= new.hi <= old.hi: old.hi = new.hi; overlaps = True
+      if new.hi >= old.hi and old.lo <= new.lo <= old.hi: old.lo = new.lo; overlaps = True
+    if not overlaps: b4 += [new]
+  return rule
 
-def selects(criteria,row):
-  def OR(criterions):
-    for criterion in criterions:
-      x = row[criterion.at]
-      if x=="?" or criterion.lo <= x <= criterion.hi: 
-        return True
-  for criterions in criteria.all.values():
-    if not OR(criterions): return False
+def selects(rule,row):
+  def select1(keys):
+    for key in keys:
+      x = row[key.at]
+      if x=="?" or key.lo <= x <= key.hi: return True
+  for keys in rule.cols.values():
+    if not select1(keys): return False
   return True
 
-def criteria(bests,rests,B=1,R=1):
-  for best,rest in zip(bests.cols.x, rests.cols.x):
-    for criterion  in differences(best,rest):
-      bs  = selects(best.rows, [criterion])
-      rs  = selects(rest.rows, [criterion])
-      b,r = len(bs)/B, len(rs)/R
-      criterion.score =  b**2/(b+r)
-      yield criterion
+def score(rule, bests, rests, B=1, R=1):
+  bs  = [row for row in best.rows if selects(rule,row)]
+  rs  = [row for row in rest.rows if selects(rule,row)]
+  b,r = len(bs)/B, len(rs)/R
+  rule.score =  b**2/(b+r)
+  yield rule
 
-def differences(best,rest):
+def rules(bests,rests,B=1,R=1):
+  for best,rest in zip(bests.cols.x, rests.cols.x):
+    for key in keys(best,rest):
+      yield score(RULE( [key] ), bests,rests,B,R)
+
+def keys(best,rest):
   "Returns ranges (lo,hi) being values more often in `best` than `rest`."
   def sym():
     freq = lambda col,l: col.has.get(k,0) / col.n
-    return [k,k for k in best.has if freq(best,k) > freq(rest,k)]
+    return [(k,k) for k in best.has if freq(best,k) > freq(rest,k)]
   def num():
     mid  = guassianIntersection(best.mu,rest.mu,div(best), div(rest))
     a, z, mu, d = -1E30, 1E30, best.mu, abs(best.mu - mid)
     tmp  = [(mu-d, mu+d), (mu-d/2, mu+d/2)]
     tmp += ([(a, mu),(a, mu+d/2),(a, mu+d)] if mu < rest.mu else [(mu, z),(mu-d/2, z),(mu-d, z)])
     f    = best.filter
-    return [(f(lo), f(hi)) for lo,hi in tmp]
+    return set(( (f(lo), f(hi)) for lo,hi in tmp ))
   #----------------------------------------
-  for lo,hi in set( sym() if best.this is SYM else num() ):
-    yield CRITERION(best,lo,hi)
+  for lo,hi in ( sym() if best.this is SYM else num() ):
+    yield KEY(best,lo,hi)
 
-def selects(rows, criteria):
-  return [row for row in rows if not rejects(row,criteria)]
-
-def rejects(row,criteria):
-  for criterion in criteria:
-
-#----------------------------------------------------------------------------------------
 def bore(bests,rests,b4=0,stop=None,B=None,R=None):
   stop = stop or len(bests.rows)**the.min
   B,R  = B or len(bests.rows), R or len(rests.rows)
   if stop < len(bests.rows):
     criterion = sorted(criteria(bests,rests,B,R),reverse=True,key=lambda c:c.score)[0] 
     if criterion,score > b4:
-      print(BAG(b=len(bests.rows), r=len(rests.rows),name=name,lo=lo,hi=hi,score=f"{score:3g}"))
+      print(BOX(b=len(bests.rows), r=len(rests.rows),name=name,lo=lo,hi=hi,score=f"{score:3g}"))
       bestrows = [row for row in bests.rows if select(row[at],lo,hi)]
       restrows = [row for row in rests.rows if select(row[at],lo,hi)]
       if len(bestrows) != len(bests.rows) or len(restrows) != len(rests.rows):
