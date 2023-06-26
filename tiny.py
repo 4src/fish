e17a27035395879a769c0b14a79184d69ffe2aca
Tim Menzies <timm@ieee.org>
2023-06-26 19:04:16 -0400

    saving

--- tiny.py
+++ tiny.py
@@ -7,28 +7,27 @@ from termcolor import colored
 from functools import cmp_to_key
 from ast import literal_eval
 
-#class BOX(object):
 class BOX(object): 
   def __init__(i,**d): i.__dict__.update(**d)
   def __repr__(i):
-    show= lambda x: x.__name__ if callable(x) else (f"{x:3g}" if isinstance(x,float) else x)
-    d = i.__dict__
+    show = lambda x: x.__name__ if callable(x) else (f"{x:3g}" if isinstance(x,float) else x)
+    d    = i.__dict__
     return "{"+" ".join([f":{k} {show(d[k])}" for k in d if  k[0] != "_"])+"}"
 
-the=BOX(seed = 1234567891, 
-        min  = .5,
-        rest = 3,
-        file = "../data/auto93.csv")
+the = BOX(seed = 1234567891, 
+          min  = .5,
+          rest = 3,
+          file = "../data/auto93.csv")
 #----------------------------------------------------------------------------------------
-def COL(at=0,name=""):
+def COL(at=0, name=""):
   return (NUM if name and name[0].isupper() else SYM)(at=at, name=name)
 
-def SYM(at=0,name=""):
-  return BOX(this=SYM, name=name, at=at, n=0, has={}, most=0, mode=None, filter=lambda x:x)
+def SYM(at=0, name=""):
+  return BOX(this=SYM, name=name, at=at, n=0, has={}, most=0, mode=None, prep=lambda x:x)
 
-def NUM(at=0,name=""):
-  return BOX(this=NUM, name=name, at=at, n=0, mu=0, m2=0, hi= -1E30, lo=1E30, 
-             filter=int,
+def NUM(at=0, name=""):
+  return BOX(this=NUM, name=name, at=at, n=0, mu=0, m2=0, 
+             hi= -1E30, lo=1E30, prep=int,
              want= 0 if (name and name[-1]) =="-" else 1)
 
 def COLS(names):
@@ -47,21 +46,19 @@ def DATA(rows=[], data=None):
   return data
 #----------------------------------------------------------------------------------------
 def add(col,x):
-  def num():
+  def _num():
     col.lo = min(x, col.lo)
     col.hi = max(x, col.hi)
     d       = x - col.mu
     col.mu += d/col.n
     col.m2 += d*(x - col.mu)
-    if type(x) == float: col.filter = float
-  def sym():
+    if type(x) == float: col.prep = float
+  def _sym():
     tmp = col.has[x] = 1 + col.has.get(x,0)
     if tmp > col.most: col.most, col.mode = tmp, x
-  #----------------------------------------
   if x != "?":
     col.n += 1
-    num() if col.this is NUM  else sym()
-  return x
+    _num() if col.this is NUM  else _sym()
 
 def norm(col,x):
   return x if (x=="?" or col.this is SYM) else (x-col.lo)/(col.hi-col.lo + 1/1E30)
@@ -70,25 +67,27 @@ def mid(col):
   return col.mode if col.this is SYM else col.mu
 
 def div(col):
-  def entropy(ns): return -sum((n/col.n*math.log(n/col.n,2) for n in ns))
-  def stdev()    : return (col.m2/(col.n-1))**.5
-  #----------------------------------------
-  return stdev() if col.this is NUM else entropy(col.has.values()) 
+  def _ent(ns) : return -sum((n/col.n*math.log(n/col.n,2) for n in ns))
+  def _std()   : return (col.m2/(col.n-1))**.5
+  return _std() if col.this is NUM else _ent(col.has.values()) 
 
-def guassianIntersection(m1,m2,std1,std2):
-  if std1==0 or std2==0 or std1==std2: return (m1+m2)/2
-  if m2 < m1: return valleyBetween(m2,m1,std2,std1)
-  a = 1/(2*std1**2) - 1/(2*std2**2)
-  b = m2/(std2**2) - m1/(std1**2)
-  c = m1**2 /(2*std1**2) - m2**2 / (2*std2**2) - math.log(std2/std1)
-  root1= (-b + (b**2 - 4*a*c)**.5)/(2*a)
-  return root1 if  m1 <= root1 <= m2 else (-b - (b**2 - 4*a*c)**.5)/(2*a)
+def cross(num1,num2)
+  mu1, std1 = num1.mu, div(num1)
+  mu2, std2 = num2.mu, div(num2)
+  if mu2 < mu1: return cross(num2, num1)
+  if std1==0 or std2==0 or std1==std2: return (mu1+mu2)/2
+  a  = 1/(2*std1**2) - 1/(2*std2**2)
+  b  = mu2/(std2**2) - mu1/(std1**2)
+  c  = mu1**2 /(2*std1**2) - mu2**2 / (2*std2**2) - math.log(std2/std1)
+  x1 = (-b + (b**2 - 4*a*c)**.5)/(2*a)
+  x2 = (-b - (b**2 - 4*a*c)**.5)/(2*a)
+  return x1 if mu1 <= x1 <= mu2 else x2
 #----------------------------------------------------------------------------------------
 def adds(data, row):
-  def create(): data.cols  = COLS(row)
-  def update(): data.rows += [[add(col,row[col.at]) for col in data.cols.all]]
-  #----------------------------------------
-  return update() if data.cols else create()
+  def _create(): data.cols = COLS(row)
+  def _update(): [add(col,row[col.at]) for col in data.cols.all]
+                 data.rows += [row]
+  _update() if data.cols else _create()
 
 def d2h(data,row):
   return (sum((col.want - norm(col,row[col.at]))**2 for col in data.cols.y) / len(data.cols.y))*.5
@@ -112,7 +111,7 @@ def RULE(keys=[]):
 
 def key(rule, new):
   if new.this is RULE:
-    [key(rule,k) for lst in new.cols.values() for k in lst]
+    [key(rule, k) for keys in new.cols.values() for k in keys]
   else:
     b4 = rule.cols[new.at] = rule.cols.get(new.at,[])
     overlaps = False
@@ -123,40 +122,37 @@ def key(rule, new):
   return rule
 
 def selects(rule,row):
-  def select1(keys):
+  def _or(keys):
     for key in keys:
       x = row[key.at]
       if x=="?" or key.lo <= x <= key.hi: return True
   for keys in rule.cols.values():
-    if not select1(keys): return False
+    if not _or(keys): return False
   return True
 
 def score(rule, bests, rests, B=1, R=1):
   bs  = [row for row in best.rows if selects(rule,row)]
   rs  = [row for row in rest.rows if selects(rule,row)]
   b,r = len(bs)/B, len(rs)/R
-  rule.score =  b**2/(b+r)
+  rule.score = b**2/(b+r)
   yield rule
 
 def rules(bests,rests,B=1,R=1):
   for best,rest in zip(bests.cols.x, rests.cols.x):
     for key in keys(best,rest):
-      yield score(RULE( [key] ), bests,rests,B,R)
+      yield score(RULE([ key ]), bests,rests,B,R)
 
 def keys(best,rest):
   "Returns ranges (lo,hi) being values more often in `best` than `rest`."
-  def sym():
+  def _sym():
     freq = lambda col,l: col.has.get(k,0) / col.n
     return [(k,k) for k in best.has if freq(best,k) > freq(rest,k)]
-  def num():
-    mid  = guassianIntersection(best.mu,rest.mu,div(best), div(rest))
-    a, z, mu, d = -1E30, 1E30, best.mu, abs(best.mu - mid)
-    tmp  = [(mu-d, mu+d), (mu-d/2, mu+d/2)]
-    tmp += ([(a, mu),(a, mu+d/2),(a, mu+d)] if mu < rest.mu else [(mu, z),(mu-d/2, z),(mu-d, z)])
-    f    = best.filter
-    return set(( (f(lo), f(hi)) for lo,hi in tmp ))
-  #----------------------------------------
-  for lo,hi in ( sym() if best.this is SYM else num() ):
+  def _num():
+    a,z,mu,d = -1E30, 1E30, best.mu, abs(best.mu - cross(best.mu,rest.mu,div(best), div(rest)))
+    tmp = [(a, mu),(a, mu+d/2),(a, mu+d)] if mu < rest.mu else [(mu, z),(mu-d/2, z),(mu-d, z)]
+    f   = best.prep
+    return [ (f(lo), f(hi)) for lo,hi in tmp + [(mu-d, mu+d), (mu-d/2, mu+d/2)] ]
+  for lo,hi in set( _sym() if best.this is SYM else _num() ):
     yield KEY(best,lo,hi)
 
 def bore(bests,rests,b4=0,stop=None,B=None,R=None):
