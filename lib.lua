adbee18c4f83aca90abd69bdcb3c837335430cd7
Tim Menzies <timm@users.noreply.github.com>
2023-06-29 17:21:15 -0400

    saving

--- lib.lua
+++ lib.lua
@@ -3,48 +3,47 @@
 -- In this code, in function argument lists, two spaces denotes "start of    
 -- optional args" and four spaces denotes "start of local variables". Also:
 --
--- | `i.`   |=| reference to self |
+-- | this   |=| reference to that |
 -- |-------:|-|-------------------|
 -- | `t`    |=| table | 
--- | `u`    |=| another table (often generated from `t`) |
 -- | `n`    |=| number |  
 -- | `s`    |=| string |
 -- | `x`    |=| anything |
 -- | `k,v`  |=| key,value |
 -- | `fun`  |=| function |
 -- | `_fun` |=| a local function inside the current function |
--- | `Xs`   |=| list of X  (so `ss` is a list of strings) |
+-- | `Xs`   |=| list of X  (so `ss` is a list of strings, `ts`` is a list of tables, etc) |
 -- | `X1`   |=| an example of X (so `s1` is a string) |
 -- | `abc`  |=| (if upper case `ABC` is a constructor), an instance of class `ABC` |
 
-local l={}
+local lib={}
 
 -- ### Linting the code
 local b4={}; for k,_ in pairs(_ENV) do b4[k] = k end
-function l.rogues()
+function lib.rogues()
   for k,_ in pairs(_ENV) do if not b4[k] then 
     io.stderr:write("-- warning: rogue local [",k,"]\n") end end end
 
 -- ### Short-cuts   
 
 -- A big number 
-l.big = 1E30
+lib.big = 1E30
 -- emulate C's printf
-l.fmt = string.format
+lib.fmt = string.format
 
 -- ### Maths
 
 -- Rounds `n` to `nPlaces` (default=2)
-function l.rnd(n,  nPlaces,     mult)
+function lib.rnd(n,  nPlaces,     mult)
   mult = 10^(nPlaces or 2)
   return math.floor(n * mult + 0.5) / mult end
 
 -- Generate random numbers.
 local Seed = 937162211
 -- Returns random integers `nlo` to `nhi`.
-function l.rint(nlo,nhi)  return math.floor(0.5 + l.rand(nlo,nhi)) end
+function lib.rint(nlo,nhi)  return math.floor(0.5 + lib.rand(nlo,nhi)) end
 -- Returns random floats `nlo` to `nhi` (defaults 0 to 1)
-function l.rand(nlo,nhi) 
+function lib.rand(nlo,nhi) 
   nlo, nhi = nlo or 0, nhi or 1
   Seed = (16807 * Seed) % 2147483647
   return nlo + (nhi-nlo) * Seed / 2147483647 end
@@ -55,66 +54,69 @@ function l.rand(nlo,nhi)
 -- accepts an item's index _and_ the item). If `fun2` returns two values,
 -- use the second as the key for the new list (else just number the items 
 -- numerically).
-function l.kap(t, fun2,    u) 
-  u={}; for k,v in pairs(t or {}) do v,k=fun2(k,v); u[k or (1+#u)]=v; end; return u end
+function lib.kap(t1, fun2,    t2) 
+  t2={}; for k,v in pairs(t1 or {}) do v,k=fun2(k,v); t2[k or (1+#u)]=v; end; return t2 end
 -- Returns a copy of `t` with all items filtered via `fun`.
-function l.map(t, fun) return l.kap(t, function(_,x) return fun(x) end) end
+function lib.map(t, fun) return lib.kap(t, function(_,x) return fun(x) end) end
 
 -- ### Lists
 
 -- Sorts `t` using `fun`, returns `t`.
-function l.sort(t,fun) table.sort(t,fun); return t end
+function lib.sort(t,fun) table.sort(t,fun); return t end
 -- Returns the  keys of list `t`, sorted.
-function l.keys(t) return l.sort(l.kap(t,function(k,_) return k end)) end
+function lib.keys(t) return lib.sort(lib.kap(t,function(k,_) return k end)) end
 -- Returns `x` after pushing onto `t`
-function l.push(t,x) t[#t+1]=x; return x end
+function lib.push(t,x) t[#t+1]=x; return x end
 
 -- ### Strings
 
 -- Convert `s` into an integer, a float, a bool, or a string (as appropriate). Return the result.
-function l.coerce(s,    _fun)
+function lib.coerce(s,    _fun)
   function _fun(s1)
     return s1=="true" and true or (s1 ~= "false" and s1) or false end
   return math.tointeger(s) or tonumber(s) or _fun(s:match"^%s*(.-)%s*$") end
 
 -- Return a string  showing `t`'s contents (recursively), sorting on the keys.
-function l.o(t,     _fun,pre) 
+function lib.o(t,     _fun,pre) 
   if type(t)~="table" then return tostring(t) end
-  _fun = function(k,v) return l.fmt(":%s %s",k,l.o(v)) end 
+  _fun = function(k,v) return lib.fmt(":%s %s",k,lib.o(v)) end 
   pre = t.a or ""
-  return pre.."{"..table.concat(#t>0  and l.map(t,l.o) or l.sort(l.kap(t,_fun))," ").."}" end
+  return pre.."{"..table.concat(
+                     #t>0  and lib.map(t,lib.o) or lib.sort(lib.kap(t,_fun)),
+                     " ").."}" end
 
 -- Print `t` (recursively) then return it.
-function l.oo(t) print(l.o(t)); return t end
+function lib.oo(t) print(lib.o(t)); return t end
 
 -- Return `t`, updated from the command-line.  For `k,v` in
 -- `t`,if the command line mentions key `k` then change `s` to a new
 -- value.  If the old value is a boolean, just flip the old. 
-function l.cli(t,help)
+function lib.cli(t,help)
   for k,v in pairs(t) do
     v = tostring(v)
     for n,x in ipairs(arg) do
       if x=="-"..(k:sub(1,1)) or x=="--"..k then
         v = v=="false" and "true" or v=="true" and "false" or arg[n+1] end end 
-    t[k] = l.coerce(v) end
+    t[k] = lib.coerce(v) end
   if t.help then os.exit(print(help)) end
   return t end
 
 -- Parse `help` text to extract settings.
-function l.settings(s,       t)
+function lib.settings(s,       t)
   t={}
   s:gsub("\n[%s]+[-][%S][%s]+[-][-]([%S]+)[^\n]+= ([%S]+)",
-         function(k,v) t[k]=l.coerce(v) end)
+         function(k,v) t[k]=lib.coerce(v) end)
   return t end
 
 -- ### Klasses
 
 -- Create a klass and a constructor and a print method
-function l.obj(s,    t) 
-  t = {__tostring = function(s) return l.o(s) end}
+function lib.obj(s,    t) 
+  t = {__tostring = function(s) return lib.o(s) end}
   t.__index = t
-  return setmetatable(t, {__tostring=l.o, __call=function(_,...) 
-    local i=setmetatable({a=s},t); return setmetatable(t.new(i,...) or i,t) end}) end
+  return setmetatable(t, {__tostring=lib.o, __call=function(_,...) 
+    local self=setmetatable({a=s},t); 
+    return setmetatable(t.new(self,...) or self,t) end}) end
 
 -- ### Test Engine
 
@@ -122,37 +124,32 @@ local egs={}
 -- `eg("fred",function() return 1==2 end)` creates a test
 -- called _fred_ which can be called from the command-line using
 -- `-g fred`.
-function l.eg(s,fun) egs[1+#egs] = {name=s, fun=fun} end
+function lib.eg(s,fun) egs[1+#egs] = {name=s, fun=fun} end
 
 -- Show the help (if asked to).  Run one test, or if the test is `all`, 
 -- then run all (printing "FAIL" or "PASS" as you go).  Check for stray i
 -- globals.  Return to the operating system the number of failures 
 -- (so zero means "everything is ok").
-function l.run(settings,    fails,saved)
-  fails,saved=0,{}
-  for k,v in pairs(settings) do saved[k]=v end
+function lib.run(settings,     fails,old,fail,pass,report,dump,reset)
+  old = {}
+  for k,v in pairs(settings) do old[k]=v end
+  fail=   function(s,msg) print(lib.fmt("❌ FAIL %s %s",s,msg or "")) end
+  pass=   function(s)     print(lib.fmt("✅ PASS %s",s)) end 
+  report= function()      print(lib.fmt("🔆 failure(s) = %s",fails)) end
+  dump=   function()      print(debug.traceback()) end
+  reset=  function()      for k,v in pairs(old) do settings[k]=v end  
+                          Seed = settings.seed
+                          math.randomseed(Seed) end
+  fails = 0
   for _,sfun in pairs(egs) do
-    local s, fun = sfun.name, sfun.fun
-    if settings.go == s or settings.go == "all" then
-      local failed, also = l.failure(saved,settings,fun)
-      if   failed 
-      then fails = fails + 1
-           print(l.fmt("❌ FAIL %s %s",s,also)) 
-      else print(l.fmt("✅ PASS %s",s)) end end end
-  print(l.fmt("🔆 failure(s) = %s",fails)) 
-  l.rogues()
+    if settings.go == sfun.name or settings.go == "all" 
+    then reset()
+         local ok,val = pcall(sfun.fun)
+         if not ok then         dump(); fail(sfun.name,val); fails= fails+1
+         elseif val==false then         fail(sfun.name);     fails= fails+1
+         else pass() end end end
+  report()
+  lib.rogues()
   os.exit(fails) end
 
--- Return `true,message` if `fun` fails. Before running,
--- reset `settings` to the saved values. Reset random number
--- generators. Print a stacktrace of there is a failure.
-function l.failure(saved,settings,fun,     ok,val)
-  for k,v in pairs(saved) do settings[k]=v end 
-  Seed = settings.seed
-  math.randomseed(Seed)
-  ok,val = pcall(fun)
-  if not ok then print(debug.traceback()); return true, val
-  elseif val==false then return true, "test returned false"
-  else return false, "all good" end end
-
-return l
+return lib
--- tiny.lua
+++ tiny.lua
@@ -29,21 +29,21 @@ function COL(n,s)
 -- ### SYM
 
 -- `SYM`s uses `has` to count symbols seen so far (and the most common symbol is the `mode`).
-function SYM.new(i,n,s) 
+function SYM:new(n,s) 
   return {name=s or "", at=n or 0, n=0, has={}, most=0, mode=nil}  end
 
 -- Update a `SYM`bol.
-function SYM.add(i,x)
+function SYM:add(x)
   if x=="?" then return end
-  i.n      = 1 + i.n
-  i.has[x] = 1 + (i.has[x] or 0)
-  if i.has[x] > i.most then i.most,i.mode = i.has[x],x end end
+  self.n      = 1 + self.n
+  self.has[x] = 1 + (self.has[x] or 0)
+  if self.has[x] > self.most then self.most,self.mode = self.has[x],x end end
 
 -- `mid,div` returns central tendency  and diversity.
-function SYM.mid(i,  _) return i.mode end
-function SYM.div(i,  nPlaces,     e) 
+function SYM:mid(_) return self.mode end
+function SYM:div(  nPlaces,     e) 
   e = 0
-  for _,v in pairs(i.has) do e = e - v/i.n * math.log(v/i.n,2) end
+  for _,v in pairs(self.has) do e = e - v/self.n * math.log(v/self.n,2) end
   return nPlaces and rnd(e,nPlaces) or e end
 -------------------------------------------------------------------------------
 -- ### NUM
@@ -53,31 +53,31 @@ function SYM.div(i,  nPlaces,     e)
 -- Also, `pretty` controls how we report numbers (and this switches to "%g" 
 -- if we ever see a float).  Any name ending with `-` is something we want to _minimize_ 
 -- (so we give it a non-positive weight).
-function NUM.new(i,n,s) 
+function NUM:new(n,s) 
   return {name=s or "",  at=n or 0,  n=0, 
           lo=l.big, hi= -l.big, mu=0,  m2=0, 
           pretty="%.0f", want=(s or ""):find"-$" and 0 or 1} end
 
 -- Update a `NUM`ber.
-function NUM.add(i,n,    d)
+function NUM:add(n,    d)
   if n=="?" then return end
-  i.n  = 1 + i.n
-  i.lo = math.min(n, i.lo)
-  i.hi = math.max(n, i.hi)
-  d    = n - i.mu
-  i.mu = i.mu + d/i.n
-  i.m2 = i.m2 + d*(n - i.mu)
-  if math.type(n) == "float" then i.pretty = "%g" end end
+  self.n  = 1 + self.n
+  self.lo = math.min(n, self.lo)
+  self.hi = math.max(n, self.hi)
+  d    = n - self.mu
+  self.mu = self.mu + d/self.n
+  self.m2 = self.m2 + d*(n - self.mu)
+  if math.type(n) == "float" then self.pretty = "%g" end end
 
 -- `mid,div` returns central tendency  and diversity.
-function NUM.mid(i,  nPlaces) 
-  return nPlaces and rnd(i.mu,nPlaces) or i.mu end
-function NUM.div(i,  nPlaces,      sd)
-  sd = (i.m2/(i.n - 1))^.5; return nPlaces and rnd(sd,nPlaces) or sd end
+function NUM:mid(  nPlaces) 
+  return nPlaces and rnd(self.mu,nPlaces) or self.mu end
+function NUM:div(  nPlaces,      sd)
+  sd = (self.m2/(self.n - 1))^.5; return nPlaces and rnd(sd,nPlaces) or sd end
 
 -- Return `n` mapped 0..1, min..max.
-function NUM.norm(i,n)
-  return n=="?" and x or (n - i.lo)/(i.hi - i.lo + 1/l.big) end
+function NUM:norm(n)
+  return n=="?" and x or (n - self.lo)/(self.hi - self.lo + 1/l.big) end
 
 local function cross(mu1,mu2,sd1,sd2)
   local sd1,sd2,a,b,c,x1,x2
@@ -89,57 +89,54 @@ local function cross(mu1,mu2,sd1,sd2)
   x1 = (-b + (b^2 - 4*a*c)^.5)/(2*a)
   x2 = (-b - (b^2 - 4*a*c)^.5)/(2*a)
   return mu1 <= x1 <= mu2 and x1 or x2 end
-
-
-
 -------------------------------------------------------------------------------
 -- ### COLS
 
 -- `COLS` convert  list of column names (as strings) into `SYM`s or `NUM`s. 
 -- Column names ending in a goal symbol (`+-!`) are dependent `y` features 
 -- (and the others are independent `x` features). 
-function COLS.new(i,ss) 
-  i.x, i.y, i.all, i.names = {},{},{},ss
+function COLS:new(ss) 
+  self.x, self.y, self.all, self.names = {},{},{},ss
   for n,s in pairs(ss) do
-    local col = l.push(i.all, COL(n,s))
+    local col = l.push(self.all, COL(n,s))
     if not s:find"X$" then 
-      l.push(s:find"[!+-]$" and i.y or i.x, col)
-      if s:find"!$" then i.klass=col end  end end  end
+      l.push(s:find"[!+-]$" and self.y or self.x, col)
+      if s:find"!$" then self.klass=col end  end end  end
 
 -- Update a `COL` with info from `t`.
-function COLS.add(i,t)
-  for _,cols in pairs{i.x, i.y} do
+function COLS:add(t)
+  for _,cols in pairs{self.x, self.y} do
     for _,col in pairs(cols) do 
       col:add(t[col.at]) end end 
   return t end
 
 -------------------------------------------------------------------------------
 -- ### DATA
-function DATA.new(i,rows)
-  i.rows,i.cols = {},nil
-  for _,row in pairs(rows or {}) do i:add(row) end
+function DATA:new(  ts)
+  self.rows,self.cols = {},nil
+  for _,t in pairs(ts or {}) do self:add(t) end
 
-function DATA.add(i,t)
-  if i.cols then push(i.rows, i.cols:add(t)) else i.cols = COLS(t) end end
+function DATA:add(t)
+  if self.cols then push(self.rows, self.cols:add(t)) else self.cols = COLS(t) end end
 
-function DATA.clone(i,rows,     j)
-  j = DATA()
-  j:add{i.cols.names}
-  for _,row in pairs(rows or {}) do j:add(row) end
-  return j end
+function DATA:clone(  ts,     j)
+  data1 = DATA()
+  data1:add{self.cols.names}
+  for _,t in pairs(ts or {}) do data1:add(t) end
+  return data1 end
 
-function DATA.d2h(i,t,    d)
+function DATA:d2h(t,     d)
   d = 0
-  for _,col in pairs(i.cols.y) do d = d + (col.want - col:norm(t[col.at]))^2 end
+  for _,col in pairs(self.cols.y) do d = d + (col.want - col:norm(t[col.at]))^2 end
   return d^.5 end
 
-function DATA.ordered(i)
-  return sort(i.rows, function(t,u) return i:d2h(t) < i:d2h(u) end) end
+function DATA:ordered()
+  return sort(self.rows, function(t1,t2) return self:d2h(t1) < self:d2h(t2) end) end
 
-function DATA.stats(i,fun,cols)
-  tmp=map(cols or i.cols.y, function(col) 
+function DATA:stats(fun,cols)
+  tmp=map(cols or self.cols.y, function(col) 
  
-function DATA:stats(i,  what,cols,nPlaces,     t)
+function DATA:stats(  what,cols,nPlaces,     t)
   t = {N=#self.rows}
   for _,col in pairs(cols or self.cols.all) do
     t[col.txt] =rnd(getmetatable(col)[what or "mid"](col),nPlaces) end
