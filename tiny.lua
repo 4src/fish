603554431582cd1d033a30cc7f181a220b3211fb
Tim Menzies <timm@ieee.org>
2023-07-01 13:36:23 -0400

    saving

--- tiny.lua
+++ tiny.lua
@@ -17,7 +17,7 @@ OPTIONS:
 
 local eg, o, obj, oo, rnd = l.eg, l.o, l.obj, l.oo, l.rnd
 local SYM,NUM,COLS,DATA = obj"SYM", obj"NUM", obj"COLS", obj"DATA"
-local CONDTION,RULE = obj"CONDITION", obj"RULE"
+local TEST,RULE = obj"TEST", obj"RULE"
 local COL
 -------------------------------------------------------------------------------
 -- ### COLS
@@ -80,25 +80,24 @@ function NUM:div(  nPlaces,      sd)
 function NUM:norm(n)
   return n=="?" and x or (n - self.lo)/(self.hi - self.lo + 1/l.big) end
 
-function SYM.keys1(i,j,     t)
+-- Return symbols more frequent in  `sym1` than `sym2`.
+function SYM.keys(sym1,sym2,     t)
   t = {}
-  for k,n1 in pairs(i.has) do if n1/i.n < (j.has[k] or 0)/j.n then l.push(t,k) end end
+  for k,n1 in pairs(sym1.has) do 
+    if n1/sym1.n > (sym2.has[k] or 0)/sym2.n then l.push(t,k) end end
   return t end
 
-% sets keys
-% scores
-function NUM.keys1(i,j)
+-- Return numeric ranges frequent in  `sym1` than `sym2`.
+function NUM.keys(num1,num2,     a,z,mu,mid,d,tmp)
   a, z, mu = -1E30, 1E30, best.mu
-  d = math.abs(best.mu - cross(best.mu, rest.mu, best:div(), rest:div()))
+  mid = cross(num1.mu, num2.mu, num1:div(), num2:div())
+  d   = math.abs(num1.mu - mid)
   tmp = mu < rest.mu and {{a, mu},{a, mu+d/2},{a, mu+d}} or {{mu, z},{mu-d/2, z},{mu-d, z}}
-  for _,x in pairs{{mu-d, mu+d}, {mu-d/2, mu+d/2}} do l.push(tmp,x) end
-  f = function(n) return l.fmt(best.pretty,n) end 
-  return map(tmp,function(x) return {f(x[1]), f(x[2])} end)
+  for _,x in pairs{{mu-d, mu+d}, {mu-d/2, mu+d/2}} do 
+    l.push(tmp,x) end
+  return tmp end
 
-
-
-local function cross(mu1,mu2,sd1,sd2)
-  local sd1,sd2,a,b,c,x1,x2
+local function cross(mu1,mu2,sd1,sd2,      a,b,c,x1,x2)
   if mu2 < mu1 then return cross(mu2,mu1,sd2,sd1) end
   if sd1==0 or sd2==0 or sd1==sd2 then return (mu1+mu2)/2 end
   a  = 1/(2*sd1^2) - 1/(2*sd2^2)
@@ -158,26 +157,33 @@ function DATA:stats(  what,cols,nPlaces,     t)
     t[col.name] =rnd(getmetatable(col)[what or "mid"](col),nPlaces) end
   return t end
 -------------------------------------------------------------------------------
-function TEST:new(col,lo,hi)
-  return {at=col.at, b=0, r=0,lo=lo, hi=hi, name=col.name} end
+-- ### TEST
 
+-- `TEST`s ask if a range `lo..hi` covers a  value `at` some column.
+function TEST:new(col,lo,hi)
+  return {at=col.at, b=0, r=0, lo=lo, hi=hi, name=col.name} end
+
+-- Tests for coverage of data in `t`.
 function TEST:covers(t)
   local x = t[self.at]
   if x=="?" or self.lo <= x and x <= self.hi then return t end end 
 
+-- `RULE`s ask if a ranges in many `cols` covers some value
 function RULE:new(tests)
   self.cols, self.b, self.r = {}, 0,0,0,0
-  for _,test is pairs(tests or {}) do self:add(test) end end
+  for _,test in pairs(tests or {}) do self:add(test) end end
 
-  -- disjunct, spread the area XXX
+-- disjunct, spread the area XXX
+-- `TEST`s ask if a range `lo..hi` covers a  value `at` some column.
 function RULE:add(test,      olds,skip)
-  olds = self.cols[new.at] = self.cols[new.at] or {}
+  olds = self.cols[test.at] or {}
   skip = false
   for _,old in pairs(olds) do
     local a0, z0, a1, z1 = old.lo, old.hi, test.lo, test.hi
-    if z1 >= a0 and z1 <= z0 and a0 <= a1 then old.hi = test.hi; skip = true end 
-    if a1 >= a0 and a1 <= z0 and z1 >= z0 then old.lo = test.lo; skip = true end end
-  if not skip then l.push(olds,new) end end
+    if z1 >= a0 and z1 <= z0 and a0 <= a1 then old.lo = test.lo; skip = true end 
+    if a1 >= a0 and a1 <= z0 and z1 >= z0 then old.hi = test.hi; skip = true end end
+  if not skip then l.push(olds,new) end 
+  self.cols[test.at] = olds end
 
 function RULE:covers(t,    _ors)
   function _ors(tests)
@@ -187,13 +193,13 @@ function RULE:covers(t,    _ors)
     if not _ors(tests) then return nil end end
   return t end
 
-function br(thing,bests,rests)
-  local B, R = #bests.rows, #rests.rows 
-  bs  = map(bests.rows, function(t) return thing:covers(t) end)
-  rs  = map(rests.rows, function(t) return thing:covers(t) end)
+local function br(thing,bests,rests,     B,R)
+  B, R = #bests.rows, #rests.rows 
+  bs   = map(bests.rows, function(t) return thing:covers(t) end)
+  rs   = map(rests.rows, function(t) return thing:covers(t) end)
   thing.b, thing.r = #bs/(B + 1/l.big),  #rs/(R + 1/l.big) end
 
-function goal(thing)
+local function goal(thing,     b,r)
   b, r = thing.b, thing.r
   if the.Goal == "plan"    then return b^2/(b+r) end
   if the.Goal == "monitor" then return r^2/(b+r) end
