db05194bff27e4a71353d1c058bfa79275b8ed20
Tim Menzies <timm@users.noreply.github.com>
2023-07-15 18:10:37 -0400

    saving

--- us.py
+++ us.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3 -B
 # <!--- vim: set et sts=3 sw=3 ts=3 : --->
 """
-S.PY: a little AI
+uspy: a little AI tool
 (c) Tim Menzies <timm@ieee.org>, BSD-2 license
 
 OPTIONS:
   -b --bins   initial number of bins     = 16
-  -c --cohen  small delta = cohen*stdev = .35
-  -f --file   whre to read data          = "../data/auto93.csv"
+  -c --cohen  small delta = cohen*stdev  = .35
+  -f --file   where to read data         = "../data/auto93.csv"
+  -g --go     start up action            = "help"
+  -h --help   show help                  = False
   -s --seed   random number seed         = 1234567890
   -m --min    minuimum size              = .5
   -r --rest   |rest| = |best|*rest       = 3
@@ -73,7 +75,7 @@ def discretize(data, bestRows,restRows):
       for cut in cuts:
          if within(x,cut): return cut
 
-   def _sym(c,a):
+   def _syms(c,a):
       "return one cut for each symbol"
       return [(c, x, x) for x in  sorted(set(a))]
 
@@ -92,31 +94,32 @@ def discretize(data, bestRows,restRows):
 
    def _counts(c,cuts):
       "count how often a cut appears in best or rest"
-      xys = {cut : obj(x=cut, y=obj(best=0, rest=0)) for cut in _cugs(x,c({}
+      xys = {cut[0] : obj(x=cut, y=obj(best=0, rest=0)) for cut in cuts}
       for y,rows in [("best",bestRows), ("rest", restRows)]:
          for row in rows:
             x = row.cells[c]
             if x != "?":
                xys[_cuts(x,cuts)].y[y] += 1
       tmp = sorted(xys.values(), key=lambda xy:xy.x[0])
-      return _merges(tmp) is isNum(data.names[c]) else tmp
+      return _merges(tmp) if isNum(data.names[c]) else tmp
 
    def _merges(ins):
-      "Stop merge when we can no longer merge one thing to its neighbor"
+      "Try merging any thing with its neighbor. Stop when no more merges found."
       outs, n = [], 0
       while n < len(ins):
-         one = ins[n]
+         thing = ins[n]
          if n < len(ins)-1:
-            if merged := _merged(one, ins[n+1]):
-               one = merged
+            neighbor = ins[n+1]
+            if merged := _merged(thing, neighbor):
+               thing = merged
                n += 1
-         outs += [one]
+         outs += [thing]
          n += 1
       return ins if len(ins)==len(outs) else _merges(outs)
 
    def _merged(xy1,xy2):
       "return a combined xy, but only if the combo is not more complex than that parts"
-      xy3 = obj(x=(xy1.x[0], xy1.x[1], xy2 x[3])
+      xy3 = obj(x=(xy1.x[0], xy1.x[1], xy2, xy[3]),
                 y=obj(best= xy1.y.best + xy2.y.best,
                       rest= xy1.y.rest + xy2.y.rest))
       n1,n2 = xy1.y.best + xy1.y.rest, xy2.y.best + xy2.y.rest
@@ -125,7 +128,7 @@ def discretize(data, bestRows,restRows):
 
    for c,name in enumerate(data.names):
       if not isGoal(name) and not isIgnored(name):
-         data.cuts[c] = _counts(c, _num(c) if isNum(name) else _sym(c))
+         data.cuts[c] = _counts(c, (_nums if isNum(name) else _syms)(c,data,cols[c]))
    return data
 
 #---------------------------------------------
@@ -147,39 +150,59 @@ def prints(*dists):
    print(*dists[0].keys(), sep="\t")
    [print(*d.values(), sep="\t") for d in dists]
 
+def updateFromCLI(d):
+   for k, v in d.items():
+      s = str(v)
+      for j, x in enumerate(sys.argv):
+         if ("-"+k[0]) == x or ("--"+k) == x:
+            d[k] = coerce("True" if s == "False" else ("False" if s == "True" else sys.argv[j+1]))
+
 #---------------------------------------------
-class EG:
-   """asdas asd asdas da sddas d adasasd
-     asdasadsdsa"""
-   ALL = locals()
-   def RUN(a=sys.argv): a[1:] and sys.exit(EG.RUN1( EG.ALL.get(a[1][2:], EG.help)))
-   def RUN1(fun):
+class GO:
+   All = locals()
+   def Now(a=sys.argv): 
+      updateFromCLI(the)
+      if the.help: sys.exit(GO.help())
+      GO.Run( GO.All.get(the.go, GO.help))
+
+   def Run(fun):
       global the
       saved = deepcopy(the)
       random.seed(the.seed)
       failed = fun()==False
+      print("❌ FAIL" if failed else "✅ OK", fun.__name__)
       the = deepcopy(saved)
       return failed
+
+   def all():
+      "run all actions, return to operating system a count of the number of failures"
+      sys.exit(sum(map(GO.Run,[fun for s,fun in GO.All.items() if s != "all" and s[0].islower()])))
+
    def help():
        "show help"
        print(__doc__); print("ACTIONS:")
-       [print(f"  --{x:9} {f.__doc__}")
-       for x,f in EG.ALL.items() if x[0].islower()]
+       [print(f"  -g {x:8} {f.__doc__}")
+       for x,f in GO.All.items() if x[0].islower()]
+
    def the():
       "show config"
       print(the)
+
    def nums():
       "can we find mean and sd of N(10,1)?"
       g= lambda mu,sd: mu+sd*sqrt(-2*log(R())) * cos(2*pi*R())
       a= sorted([g(10,1) for x in range(1000)])
       print(median(a),stdev(a))
+
    def read():
       "can we print rows from a disk-based csv file?"
       [print(row.cells) for row in csv(the.file)]
+
    def data():
       "can we load disk rows into a DATA?"
       data1 = DATA(csv(the.file))
       prints(stats(data1))
+
    def sorted():
       "can we find best, rest rows?"
       data1= DATA(csv(the.file))
@@ -188,6 +211,7 @@ class EG:
       prints(stats(data1),
              stats(clone(data1, rows[:n])),
              stats(clone(data1, rows[-n*the.rest:])))
+
    def discret():
       "can i do supervised discretization?"
       data1 = DATA(csv(the.file))
@@ -199,7 +223,7 @@ class EG:
          lst = data1.cols[c]
          print(f"{c:2} {lst[0]:8} {lst[-1]:8}",a)
 
-EG.RUN()
+GO.Now()
 
 # data = discretize(data)
 # tmp = sortedRows(data)
